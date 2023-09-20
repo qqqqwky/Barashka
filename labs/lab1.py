682785bad7bcfd0e199f82dd4a688f61fb79823d
qqqqwky <144202018+qqqqwky@users.noreply.github.com>
2023-09-20 11:07:02 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,11 +1,12 @@
 import math 
-x = input(float('Отвечай сука: '))
-y = input(float('Отвечай сука: '))
-z = input(float('Отвечай сука: '))
+daf task1():
+    x = input(float('Отвечай сука: '))
+    y = input(float('Отвечай сука: '))
+    z = input(float('Отвечай сука: '))
 
-a = (math.tan((x+1)**(1/2))-((abs(y))**4))/(x**(1/3))
+    a = (math.tan((x+1)**(1/2))-((abs(y))**4))/(x**(1/3))
 
-b = (y**(1/2))+(z**x)+(math.sinh(y))
+    b = (y**(1/2))+(z**x)+(math.sinh(y))
 
-print('a =', a)
-print('b =', b)
+    print('a =', a)  
+    print('b =', b)
