50807b3cee26e8f5c8c802a48ff71719d9190b51
qqqqwky <144202018+qqqqwky@users.noreply.github.com>
2023-09-20 11:02:41 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,11 @@
 import math 
+x = input(float('Отвечай сука: '))
+y = input(float('Отвечай сука: '))
+z = input(float('Отвечай сука: '))
 
-def task1():
-  pass
+a = (math.tan((x+1)**(1/2))-((abs(y))**4))/(x**(1/3))
+
+b = (y**(1/2))+(z**x)+(math.sinh(y))
+
+print('a =', a)
+print('b =', b)
